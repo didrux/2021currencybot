5c24d900c8bcf93bfe29abd2a78734cc685d0531
didrux <mishaov@gmail.com>
2021-05-20 16:11:50 +0300

    add smth

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ bot = telebot.TeleBot(token)
 
 #sdf
 #sdf
+#dsf
 response = requests.get('https://minfin.com.ua/api/currency/ratelist/?currency1=usd&currency2=uah&converter_type=midbank')
 json_data = json.loads(response.text)
 usd_a = json_data['data']['rates']['buy']['USD']
