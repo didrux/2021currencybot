9e276e351c6c36099992f92e6773c7071b810e0d
didrux <mishaov@gmail.com>
2021-06-01 21:52:40 +0300

    checker function another

--- main2.py
+++ main2.py
@@ -113,7 +113,7 @@ def kb_rub():
 
 def checker(message):
     if message.text == message:
-        bot.send_message(message.from_user.id, "Choose the currency you want to convert" + message + "to, than enter needed value", reply_markup=kb_usd())
+        bot.send_message(message.from_user.id, "Chose the currency you want to convert" + message + "to, than enter needed value", reply_markup=kb_usd())
 
 checker(USD_txt)
 checker(EUR_txt)
