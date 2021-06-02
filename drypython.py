8a349a99a2ed40ee4e2295cc424b9b12aabb9be6
didrux <mishaov@gmail.com>
2021-06-02 15:40:52 +0300

    dry python workable function

---
+++ drypython.py
@@ -0,0 +1,14 @@
+a=input()
+if a == 'usd':
+    print('You wrote usd value')
+if a == 'eur':
+    print('You wrote eur value')
+if a == 'gbp':
+    print('You wrote gbp value')
+
+def checker(a):
+    if a == a:
+        print("You wrote " + a + " value")
+
+checker(a)
+
